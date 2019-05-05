bf777159488b0187af8f82caf2834f5c6f217c5c
Rayla Medeiros <Rayla.Medeiiros@gmail.com>
2019-05-05 17:54:35 -0300

    Implement organization screen

---
+++ components/organization/GitHubButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <a href="https://github.com/panelinhadees/" class="button--grey">GitHub</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GitHubButton'
+}
+</script>
+<style lang="scss" scoped></style>
---
+++ pages/organization.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <div>
+      <!-- AvatarOrganization/ -->
+      <h1>NeoLite</h1>
+      <h2><a href="#">Site</a></h2>
+      <GitHubButton/>
+    </div>
+  </div>
+</template>
+
+<script>
+import GitHubButton from '@/components/organization/GitHubButton'
+//import AvatarOrganization from '@/components/commons/AvatarOrganization'
+
+export default {
+  components: {
+    GitHubButton
+    //AvatarOrganization
+  }
+}
+</script>
+
+<style scoped>
+    .container {
+        margin: 0 auto;
+        min-height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        background-color: #ffffff;
+        background-image: linear-gradient(315deg, #ffffff 0%, #d7e1ec 74%);
+    }
+
+    a {
+        text-decoration: none;
+    }
+</style>
