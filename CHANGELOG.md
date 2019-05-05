1283cf834cecde097ef8d6b4583eb3b24693512c
Thayanne Sousa <thayannevls@gmail.com>
2019-05-05 12:58:06 -0300

    change authentication type

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -16,3 +16,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 # Fixed
 
 - Reload page when Logout is clicked.
+
+# Changed
+
+- Change authentication type to default *Bearer*.
--- apollo/clientConfig.js
+++ apollo/clientConfig.js
@@ -4,7 +4,7 @@ export default function(context) {
   return {
     httpLinkOptions: {
       uri: process.env.API_URL || 'http://localhost:5000/graphql',
-      // credentials: 'same-origin',
+      credentials: 'same-origin',
       tokenName: 'authorization'
     },
     cache: new InMemoryCache()
