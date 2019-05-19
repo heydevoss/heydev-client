f6317757b41fb0ca87e70976d3b3279d2e6800c4
Mateus <mateus.oliveira@ccc.ufcg.edu.br>
2019-05-19 14:13:08 -0300

    Fix lint erros

--- components/organization/ProfileCard.vue
+++ components/organization/ProfileCard.vue
@@ -1,93 +1,93 @@
 <template>
-    <div>
-        <div id="app">
-            <el-row>
-            <el-col>
-                <el-card class = "card" :body-style="{ padding: '0px' }">
-                <br><img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
-                <div style="padding: 0px;">
-                    <span>mateusoliveira2</span>
+  <div>
+    <div id="app">
+      <el-row>
+        <el-col>
+          <el-card class="card" :body-style="{ padding: '0px' }">
+            <br /><img
+              src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+              class="image"
+            />
+            <div style="padding: 0px;">
+              <span>mateusoliveira2</span>
 
-                    <div class="description" >
-                        <br><span>aqui vai ter a descrição do user e tudo mais que der né</span><br> <br>
-                    </div>
-
-                </div>
-                <footer class = "box-footer">
-                    <table style="width:100%">
-                        <tr >
-                            <td>5</td>
-                            <td>10</td>
-                            <td>7</td>
-                        </tr>
-                        <tr>
-                            <td>Issues</td>
-                            <td>Pull Requests</td>
-                            <td>Commit</td>
-                        </tr>
-                    </table> 
-              
-                </footer>
-                </el-card>
-            </el-col>
-            </el-row>
-        </div>
+              <div class="description">
+                <br /><span
+                  >aqui vai ter a descrição do user e tudo mais que der né</span
+                ><br />
+                <br />
+              </div>
+            </div>
+            <footer class="box-footer">
+              <table style="width:100%">
+                <tr>
+                  <td>5</td>
+                  <td>10</td>
+                  <td>7</td>
+                </tr>
+                <tr>
+                  <td>Issues</td>
+                  <td>Pull Requests</td>
+                  <td>Commit</td>
+                </tr>
+              </table>
+            </footer>
+          </el-card>
+        </el-col>
+      </el-row>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'ProfileCard',
-    props: ['profile']
+  name: 'ProfileCard',
+  props: ['profile']
 }
 </script>
 
 <style>
-@import url("//unpkg.com/element-ui@2.8.2/lib/theme-chalk/index.css");
+@import url('//unpkg.com/element-ui@2.8.2/lib/theme-chalk/index.css');
 
-    table{
-        border-collapse: separate; 
-        border-spacing: 13px;
-        margin: 0 0 0 0;
-    }
-    .box-footer{
-        position:absolute;
-        align-self: center;
-        bottom:0;
-    }
-    .card{
-        width: 250px;
-        height: 350px;
-        background-color: #47525E;  
-        color: white;
-        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+table {
+  border-collapse: separate;
+  border-spacing: 13px;
+  margin: 0 0 0 0;
+}
+.box-footer {
+  position: absolute;
+  align-self: center;
+  bottom: 0;
+}
+.card {
+  width: 250px;
+  height: 350px;
+  background-color: #47525e;
+  color: white;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
 
-    }
+.description {
+  text-align: left;
+  justify-content: center;
+  margin: 3px;
+}
 
-    .description{
-        text-align:left;
-        justify-content: center;
-        margin: 3px;
-    }
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
 
-    .bottom {
-        margin-top: 13px;
-        line-height: 12px;
-    }
+.button {
+  padding: 0;
+  float: right;
+}
 
-    .button {
-        padding: 0;
-        float: right;
-    }
-
-    .image {
-        border-radius: 100%;
-        width: 100px;
-        height: 100px;
-        display: relative;
-        position: center;
-    }
-
+.image {
+  border-radius: 100%;
+  width: 100px;
+  height: 100px;
+  display: relative;
+  position: center;
+}
 </style>
-
-
