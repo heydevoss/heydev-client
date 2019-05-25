590bab87f3b3148b473ac68b40f5cbbe796197e2
Mateus <mateus.oliveira@ccc.ufcg.edu.br>
2019-05-24 22:59:19 -0300

    Update _login.vue
    tha name 'organization

--- pages/user/_login.vue
+++ pages/user/_login.vue
@@ -1,23 +1,50 @@
 <template>
   <div class="content">
     <div class="header">
-      <user-info class="user-info" />
+
+      <user-info :name="this.organization.contributor.name"
+                 :bio="this.organization.contributor.bio" 
+                 :location="this.organization.contributor.location"
+                 :avatarUrl="this.organization.contributor.avatarUrl"
+                 class="user-info" />
       <el-divider direction="vertical" class="divider" />
-      {{this.$route.params.login}}
-      <user-stats class="user-stats" />
+      {{this.userName}}
+      <user-stats :totalPullRequests="this.organization.contributor.totalPullRequests"
+                  :totalCommits="this.organization.contributor.totalCommits"
+                  :totalIssues="this.organization.contributor.totalIssues"
+                  class="user-stats" />
     </div>
   </div>
 </template>
 <script>
 import UserInfo from '@/components/user/UserInfo'
 import UserStats from '@/components/user/UserStats'
+import getContributor from '@/apollo/queries/getContributor.gql'
 
 export default {
   name: 'User',
   components: {
     UserInfo,
     UserStats
+  },
+
+  data() {
+    return {
+      userName: this.$route.params.login,
+      organization: {}
+    }
+  },
+  
+  apollo: {
+    organization: {
+      query: getContributor,
+      variables: {
+        login: "thayannevls" //this.userName ??
+      }
+    }
   }
+
+
 }
 </script>
 
