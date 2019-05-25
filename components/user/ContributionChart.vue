cdae620ede7a5b75b4e58640cd60c5ed06060225
Mateus <mateus.oliveira@ccc.ufcg.edu.br>
2019-05-25 16:45:04 -0300

    Add real data on chartjs

--- components/user/ContributionChart.vue
+++ components/user/ContributionChart.vue
@@ -1,32 +1,60 @@
 <template>
     <vue-no-ssr>
-        <div>
-            <h2>Doughnut</h2>
+        <div class="chart">
+            
+            <span class="title"> Contributions </span> 
 
-            <div class="card">
-            <chartjs-doughnut
+            <br><chartjs-doughnut
                 v-bind:bind="true"
-                v-bind:datasets="props"
+                v-bind:datasets="datasets"
                 v-bind:labels="labels"
-                v-bind:option="option"
+                v-bind:option="options"
             />
-            </div>
         </div>
     </vue-no-ssr>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      labels: ["totalPullRequests", "totalCommits", "totalIssues"],
-      option: {}
-    };
-  },
-  props: {
-    totalPullRequests: Number,
-    totalCommits: Number,
-    totalIssues: Number
-  },
+    data() {
+        return {
+            datasets: [
+                {
+                data: [ this.totalPullRequests, this.totalCommits, this.totalIssues],
+                backgroundColor: ["#b388ff", "#2196f3", "#80d8ff"],
+                hoverBackgroundColor: ["#673ab7", "#0000FF", "#00FFFF"]
+                }
+            ],
+            labels: ["Pull Requests", "Commits", "Issues"],
+            options: {
+                legend: {
+                    position: 'right'
+                }
+            }
+        }
+    },
+    props: {
+        totalPullRequests: Number,
+        totalCommits: Number,
+        totalIssues: Number
+    }
 };
 </script>
+<style>
+.title{
+    text-align: center;
+    padding: 10px;
+    font-size: 25px;
+    color: black;
+}
+
+.chart{
+    display: flex;
+    flex-direction: column;
+    align-items:baseline; 
+    width: 500px;
+    height: 500px;
+
+}
+</style>
+
